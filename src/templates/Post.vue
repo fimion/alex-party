2e664dc3db1053257c1c266e7b69d2a36af2fe68
Alex Riviere <fimion@users.noreply.github.com>
2020-09-02 23:05:29 +0000

    Add Utterances to blog posts

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -7,6 +7,7 @@
         <p>{{ $page.post.excerpt }}</p>
         <div v-html="$page.post.content"></div>
       </div>
+      <div ref="comments"/>
     </article>
   </layout>
 </template>
@@ -26,12 +27,28 @@
 <script>
   import Layout from '~/layouts/Default.vue'
   import DateDisplay from "~/components/DateDisplay.vue"
+  const UTTERANCES = `<script src=  issue-term="pathname" label="comments" theme="github-light" crossorigin="anonymous"><\/script>`;
   export default {
     name: "Post",
     components:{Layout, DateDisplay},
     metaInfo () {
       return {
-        title: this.$page.post.title
+        title: this.$page.post.title,
+      }
+    },
+    mounted(){
+      if(window){
+          const utterances = document.createElement('script');
+          utterances.src="https://utteranc.es/client.js";
+          utterances.setAttribute('repo',"fimion/alex-party");
+          utterances.setAttribute("issue-term","pathname");
+          utterances.setAttribute("label","comments");
+          utterances.setAttribute("theme","github-light");
+          utterances.setAttribute("crossorigin","anonymous");
+          utterances.setAttribute("async","async");
+          this.$nextTick(()=>{
+            this.$refs.comments.append(utterances);
+          })
       }
     }
   }
