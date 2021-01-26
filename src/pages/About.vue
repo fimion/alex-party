f9ac1c78a1d874d9ad1a8b699cfccada5c251795
fimion <fimion@gmail.com>
2021-01-25 22:02:18 -0500

    Update about page for 2021
    - add enjoy the vue
    - add resume
    - formatting

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,10 +2,21 @@
   <Layout>
     <section>
       <h1>About Me</h1>
-      <p class="sans-serif">Hi! My name is Alex Riviere. I'm a web developer from Atlanta, GA that specializes in Vue.js, python, and PHP.
-        I also help co-organize the <a href="https://www.meetup.com/Atlanta-Vue-js-Meetup/">Atlanta Vue.js Meetup</a> and the
-        <a href="https://www.meetup.com/python-atlanta/">PyATL Meetup</a> since March of 2019. This is my personal blog
-        and playground. Hopefully, something here will be helpful to you.</p>
+      <div class="sans-serif">
+        <p>
+          Hi! My name is Alex Riviere. I'm a web developer from Atlanta, GA that specializes in Vue.js, python, and PHP.
+        </p>
+        <ul>
+          <li>I help organize the <a href="https://www.meetup.com/Atlanta-Vue-js-Meetup/">Atlanta Vue.js Meetup</a></li>
+          <li>I also help organize <a href="https://www.meetup.com/python-atlanta/">PyATL</a></li>
+          <li>I'm a regular panelist on
+            <a href="https://enjoythevue.io">Enjoy the Vue</a> as of January 2021.
+          </li>
+          <li>My personal playground is over at <a href="https://icannot.design">iCannot.design.</a></li>
+          <li><a href="/uploads/Alex-Riviere-Resume-2021.01.25.pdf">Here is my resume.</a></li>
+        </ul>
+        <p>Hopefully, something here will be helpful to you.</p>
+      </div>
     </section>
   </Layout>
 </template>
@@ -13,7 +24,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
-  }
+    title: 'About us',
+  },
 }
 </script>
